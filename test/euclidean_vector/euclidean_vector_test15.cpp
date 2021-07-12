b1c1292b4a758f0aebfb59811ea4b4b491285320
Arpit Rulania <arpitkingr@example.com>
2021-07-12 21:20:03 +1000

    scalar multipy works

--- test/euclidean_vector/euclidean_vector_test15.cpp
+++ test/euclidean_vector/euclidean_vector_test15.cpp
@@ -7,11 +7,6 @@
 TEST_CASE("Operation test 1") {
 	auto a = comp6771::euclidean_vector{1.0, -2.0, -3.0, 8.0, -22.7};
 	CHECK_NOTHROW(a *= 1.2);
-	std::cout << a.at(0) << '\n';
-	std::cout << a.at(1) << '\n';
-	std::cout << a.at(2) << '\n';
-	std::cout << a.at(3) << '\n';
-	std::cout << a.at(4) << '\n';
 	CHECK(a.at(0) == Approx(1.2));
 	CHECK(a.at(1) == Approx(-2.4));
 	CHECK(a.at(2) == Approx(-3.6));
